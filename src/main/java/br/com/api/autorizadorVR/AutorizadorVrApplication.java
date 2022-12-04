288c0462162964dede3a3b5778e70aa4f4d871bc
alexandrebs <alexandrebs@users.noreply.github.com>
2022-12-04 08:43:59 -0300

    criacao projeto springboot ini
    ponto inicial do projeto

---
+++ src/main/java/br/com/api/autorizadorVR/AutorizadorVrApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.autorizadorVR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutorizadorVrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutorizadorVrApplication.class, args);
+	}
+
+}
