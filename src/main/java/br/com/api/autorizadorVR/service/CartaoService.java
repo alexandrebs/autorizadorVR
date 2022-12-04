ba60c5d959a9b58b030051dc3b57fe5a9b615694
alexandrebs <alexandrebs10@gmail.com>
2022-12-04 20:41:40 -0300

    novo metodo autorizarCartao

--- src/main/java/br/com/api/autorizadorVR/service/CartaoService.java
+++ src/main/java/br/com/api/autorizadorVR/service/CartaoService.java
@@ -2,6 +2,7 @@ package br.com.api.autorizadorVR.service;
 
 import static org.hamcrest.CoreMatchers.nullValue;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
 
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.api.autorizadorVR.dto.CartaoDTO;
+import br.com.api.autorizadorVR.dto.SaldoDTO;
 import br.com.api.autorizadorVR.exception.BadRequestException;
 import br.com.api.autorizadorVR.exception.InvalidArgumentException;
 import br.com.api.autorizadorVR.exception.NotFoundException;
@@ -32,16 +34,52 @@ public class CartaoService {
 
 	public Cartao inserirCartao(CartaoDTO dto) {
 
-		if(cartaoRepository.existsById(dto.getNumeroCartao())) {
-			throw new InvalidArgumentException("Ja existe um cartão cadastro com o número "+ dto.getNumeroCartao());
+		if (cartaoRepository.existsById(dto.getNumeroCartao())) {
+			throw new InvalidArgumentException("Ja existe um cartão cadastro com o número " + dto.getNumeroCartao());
 		}
 		Cartao cartao = new Cartao();
 		cartao = modelMapper.map(dto, Cartao.class);
 		cartao.setValor(500L);
 		cartaoRepository.save(cartao);
-		
+
 		return cartao;
 
+	}
+
+	public Object consultarSaldo(String numeroCartao) {
+
+		Object saldoDto = cartaoRepository.consultarSaldo(numeroCartao);
+
+		if (saldoDto == null) {
+			throw new NotFoundException("Saldo não enconrado para o cartão informado nº " + numeroCartao);
+		}
+
+		return saldoDto;
+	}
+
+	public Cartao autorizarCartao(Cartao cartao) {
+
+		//cartaoRepository.findById(cartao.getNumeroCartao()).orElseThrow(() -> new NotFoundException("Nenhum cartão encontrado com o número " + cartao.getNumeroCartao()));
+		
+		
+		Optional<Cartao> cartaoRetorno = cartaoRepository.findById(cartao.getNumeroCartao());
+		
+		
+		if (!cartaoRetorno.isPresent()) {
+			throw new InvalidArgumentException("CARTAO_INEXISTENTE");
+		}
+
+		if (!cartao.getSenha().equals(cartaoRetorno.get().getSenha())) {
+			throw new InvalidArgumentException("SENHA_INVALIDA");
+		}
+
+		if (cartao.getValor() > cartaoRetorno.get().getValor()) {
+			throw new InvalidArgumentException("SALDO_INSUFICIENTE");
+		}
+		cartaoRetorno.get().debitarValor(cartao.getValor()) ;
+		cartao.setValor(cartaoRetorno.get().getValor());
+		return cartaoRepository.save(cartao);
+		
 	}
 
 }
