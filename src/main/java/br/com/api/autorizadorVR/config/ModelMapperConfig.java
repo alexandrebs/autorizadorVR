3b7f0ccfad4ef8c2d7aa5b3edc6330edda2fe8ea
alexandrebs <alexandrebs10@gmail.com>
2022-12-04 15:12:08 -0300

    add arquivos

--- src/main/java/br/com/api/autorizadorVR/config/ModelMapperConfig.java
+++ src/main/java/br/com/api/autorizadorVR/config/ModelMapperConfig.java
@@ -1,5 +1,16 @@
 package br.com.api.autorizadorVR.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ModelMapperConfig {
 
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		
+		return new ModelMapper();
+	}
 }
